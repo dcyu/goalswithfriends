7a45ee46cdc16c16d94c4cfcb0230f6c0789b86a
Student <student@TSL-iMac-12.local>
2013-02-16 17:57:39 -0600

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,14 @@
 
 
 users = [
-  {:first_name => "Arif", :last_name => "Poonawala", :email => "arif.poonawala@gmail.com", :pic_url => "http://m3.licdn.com/mpr/pub/image-OSKq7rafiGdRqX-rEcrI8kD9yjQj33HvOjrQndsryi9H2bTfOSKQ2EBfy9Sy2hx3gnD_/arif-poonawala.jpg"},
-{first_name: "Dillon", last_name: "Murphy", email: "gdillonmurphy@gmail.com", pic_url: "https://lh4.googleusercontent.com/-WnsNfohi1YA/AAAAAAAAAAI/AAAAAAAAHUM/5VQB6LI6Gxc/s250-c-k/photo.jpg"},
-{first_name: "Celine", last_name: "Ui", email: "celine.ui@gmail.com", pic_url: "http://profile.ak.fbcdn.net/hprofile-ak-ash4/c127.37.466.466/s160x160/294173_10100566571572928_697166679_n.jpg"}
+  {:name => "Arif Poonawala", :email => "arif.poonawala@gmail.com", :pic_url => "http://m3.licdn.com/mpr/pub/image-OSKq7rafiGdRqX-rEcrI8kD9yjQj33HvOjrQndsryi9H2bTfOSKQ2EBfy9Sy2hx3gnD_/arif-poonawala.jpg"},
+{name: "Dillon Murphy", email: "gdillonmurphy@gmail.com", pic_url: "https://lh4.googleusercontent.com/-WnsNfohi1YA/AAAAAAAAAAI/AAAAAAAAHUM/5VQB6LI6Gxc/s250-c-k/photo.jpg"},
+{name: "Celine Ui", email: "celine.ui@gmail.com", pic_url: "http://profile.ak.fbcdn.net/hprofile-ak-ash4/c127.37.466.466/s160x160/294173_10100566571572928_697166679_n.jpg"}
 ]
 
 users.each do |user|
   u = User.new
-  u.first_name = user[:first_name]
-  u.last_name = user[:last_name]
+  u.name = user[:name]
   u.email = user[:email]
   u.pic_url = user[:pic_url]
   u.save
