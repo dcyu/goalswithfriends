54b8dae0271d50e81fc47c222df7a9b1349859b9
Daniel Yu <dcyu93@gmail.com>
2013-02-18 14:14:59 -0600

    ALL USER INFO PULLED FROM FACEBOOK!!!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.pic_url = auth.info.image
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at) unless auth.credentials.expires_at.nil?
       user.save!
